7b2e2e2159b80fc2391aca4c643d8a45e9a71c05
vinit <vinithanabar20@gmail.com>
2023-09-26 11:28:36 +0530

    added required dependencies

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Hilt_example2"
+include(":app")
+ 
